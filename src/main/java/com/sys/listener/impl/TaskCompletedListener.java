d3917874674b77cb59b61df949225c44a4e7e118
brighttang2016 <798758642@qq.com>
2016-11-29 23:20:51 +0800

    bak

--- src/main/java/com/sys/listener/impl/TaskCompletedListener.java
+++ src/main/java/com/sys/listener/impl/TaskCompletedListener.java
@@ -1,6 +1,8 @@
 package com.sys.listener.impl;
 
+import org.activiti.engine.delegate.event.ActivitiEntityEvent;
 import org.activiti.engine.delegate.event.ActivitiEvent;
+import org.activiti.engine.impl.persistence.entity.TaskEntity;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
@@ -16,6 +18,9 @@ public class TaskCompletedListener implements EventHandler {
 	@Override
 	public void handle(ActivitiEvent event) {
 		logger.debug("任务完成");
+		ActivitiEntityEvent eventImpl = (ActivitiEntityEvent) event;
+		TaskEntity taskEntity = (TaskEntity) eventImpl.getEntity();
+		logger.debug(taskEntity);
 	}
 
 }
--- src/main/java/com/sys/listener/impl/TaskCreatedListener.java
+++ src/main/java/com/sys/listener/impl/TaskCreatedListener.java
@@ -1,6 +1,9 @@
 package com.sys.listener.impl;
 
+import org.activiti.engine.delegate.event.ActivitiActivityEvent;
+import org.activiti.engine.delegate.event.ActivitiEntityEvent;
 import org.activiti.engine.delegate.event.ActivitiEvent;
+import org.activiti.engine.impl.persistence.entity.TaskEntity;
 import org.apache.log4j.Logger;
 import org.springframework.stereotype.Service;
 
@@ -16,5 +19,9 @@ public class TaskCreatedListener implements EventHandler{
 	@Override
 	public void handle(ActivitiEvent event) {
 		logger.debug("任务创建");
+		ActivitiEntityEvent eventImpl = (ActivitiEntityEvent) event;
+		TaskEntity taskEntity = (TaskEntity) eventImpl.getEntity();
+		logger.debug(taskEntity.getId()+"|"+taskEntity.getName()+"|"+taskEntity.getProcessDefinitionId()
+		+"|"+taskEntity.getTaskDefinitionKey());
 	}
 }
