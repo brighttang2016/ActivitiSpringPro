815b7838574fde4fb0ea9fa0a7e4967bb501ec8b
brighttang2016 <798758642@qq.com>
2016-11-30 00:04:38 +0800

    bak

--- src/main/java/com/sys/listener/impl/TaskCreatedListener.java
+++ src/main/java/com/sys/listener/impl/TaskCreatedListener.java
@@ -1,5 +1,7 @@
 package com.sys.listener.impl;
 
+import java.util.Map;
+
 import org.activiti.engine.delegate.event.ActivitiActivityEvent;
 import org.activiti.engine.delegate.event.ActivitiEntityEvent;
 import org.activiti.engine.delegate.event.ActivitiEvent;
@@ -21,7 +23,9 @@ public class TaskCreatedListener implements EventHandler{
 		logger.debug("任务创建");
 		ActivitiEntityEvent eventImpl = (ActivitiEntityEvent) event;
 		TaskEntity taskEntity = (TaskEntity) eventImpl.getEntity();
-		logger.debug(taskEntity.getId()+"|"+taskEntity.getName()+"|"+taskEntity.getProcessDefinitionId()
-		+"|"+taskEntity.getTaskDefinitionKey());
+		logger.debug(taskEntity.getId()+"|"+taskEntity.getName()+"|"+taskEntity.getProcessDefinitionId()+"|"+taskEntity.getTaskDefinitionKey());
+		String applyUserId = (String) taskEntity.getVariable("applyUserId");//获取运行时变量池中指定名称变量
+		Map<String,Object> varaMap = taskEntity.getVariables();//获取运行时变量池中所有变量
+		logger.debug("varaMap:"+varaMap);
 	}
 }
